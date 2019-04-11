36c45322a6b9ff1ca28dea84bca4b53a0942c464
lisheng <lisheng_token@outlook.com>
2019-04-11 10:36:40 +0800

    添加badge组件

---
+++ src/components/Badge/Badge.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="weui-badge" :class="{ 'weui-badge_dot': dot }">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeuiBadge',
+  props: {
+    dot: Boolean,
+  },
+};
+</script>
