80db1913e07fb1982d5e73337b4b9f720197eddf
lisheng <lisheng_token@outlook.com>
2019-04-11 10:25:20 +0800

    添加cell组件

---
+++ src/components/Cell/Cell.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="weui-cell"
+    :class="{ 'weui-cell_access': access }"
+    @click="cellClick">
+    <div class="weui-cell__hd">
+      <slot name="hd"></slot>
+    </div>
+    <div class="weui-cell__bd">
+      <slot name="bd"></slot>
+    </div>
+    <div class="weui-cell__ft">
+      <slot name="ft"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeuiCell',
+  props: {
+    access: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    cellClick(e) {
+      this.$emit('cellClick', e);
+    },
+  },
+};
+</script>
---
+++ src/components/Cell/Cells.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="weui-cells__title">
+      <slot name="title"></slot>
+    </div>
+    <div class="weui-cells">
+      <slot name="cells"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeuiCells',
+};
+</script>
